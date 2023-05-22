7d0da46740ba0607585980541ffab33d77fc3ea6
Paulo Guedes <paulojorge.guedes@hotmail.com>
2023-05-22 16:26:43 +0100

    Collision against walls fixed

---
+++ Assets/Scripts/CollisionDetectionPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CollisionDetectionPlayer : MonoBehaviour
+{
+
+    [SerializeField] private PlayerInput playerInput;
+
+    private void OnCollisionEnter( Collision collision ) {
+        // Debug-draw all contact points and normals
+
+        if(collision.gameObject.tag.Equals( "Floor" )) {
+
+            playerInput.RestoreJumpsAllowed();
+
+        }
+    }
+}
